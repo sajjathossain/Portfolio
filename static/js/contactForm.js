27dd35b285eaa489df6c7c6fa720825040798c65
sajjathossain <painlesssajjad@gmail.com>
2020-11-29 14:06:35 +0600

    Updated UI

--- static/js/contactForm.js
+++ static/js/contactForm.js
@@ -1,9 +1,20 @@
 const sendBTN = document.querySelector("#sendBtn");
+const senderName = document.querySelector("#senderName");
+const senderEmail = document.querySelector("#senderEmail");
+const senderBody = document.querySelector("#senderBody");
+const error = document.querySelector("#error");
 
 document.addEventListener("DOMContentLoaded",() => {
-
-    sendBTN.addEventListener("click", () => {
-        alert("Thank You! Your email was sent!");
+    error.style.display = "none";
+    sendBTN.addEventListener("click", (evt) => {
+        if(senderName.value.length <= 0 || senderEmail.value.length <= 0 || senderBody.value.length <= 0){
+            evt.preventDefault();
+            error.style.display = "block";
+            error.innerText = "Please fill up all the required fields!";
+        }else {
+            error.style.display = "none";
+            alert("Thank You! Your email was sent!");
+        }
     });
 
 });
