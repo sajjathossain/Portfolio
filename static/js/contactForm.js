c5d90f8080eb862ecf53c522c414b9b1fbfa1438
sajjathossain <painlesssajjad@gmail.com>
2020-11-29 13:53:09 +0600

    Updated UI

--- static/js/contactForm.js
+++ static/js/contactForm.js
@@ -2,7 +2,7 @@ const sendBTN = document.querySelector("#sendBtn");
 
 document.addEventListener("DOMContentLoaded",() => {
 
-    sendBTN.addEventListener("click", (e) => {
+    sendBTN.addEventListener("click", () => {
         alert("Thank You! Your email was sent!");
     });
 
